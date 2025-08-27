ec55fcc011ed4efe46bb4cf40902528931dff727
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-08-27 18:29:41 -0500

    Add country layout

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
 ];
 
