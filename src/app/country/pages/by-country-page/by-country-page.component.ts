965aac8d840faf0a3a721c933ebd1e083a7ea097
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-08-28 19:18:57 -0500

    Add by country page, by region page and country page

---
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+
+@Component({
+  selector: 'app-by-country-page',
+  imports: [SearchInputComponent, CountryListComponent],
+  templateUrl: './by-country-page.component.html',
+})
+export class ByCountryPageComponent {
+  onSearch(value: string) {
+    console.log(value);
+  }
+}
