a75451e52028048707c7375276ced1a61cf58b03
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-09-17 17:57:15 -0500

    Add by region query params

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,26 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { RegionOptionsComponent } from './region-options/region-options.component';
 import { Region } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { catchError, of } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+
+// function validateRegion(queryParam: string): Region {
+//   queryParam = queryParam.toLowerCase();
+
+//   const validRegion: Record<string, Region> = {
+//     africa: 'Africa',
+//     americas: 'Americas',
+//     asia: 'Asia',
+//     europe: 'Europe',
+//     oceania: 'Oceania',
+//     antarctic: 'Antarctic',
+//   };
+
+//   return validRegion[queryParam] ?? 'Americas';
+// }
 
 @Component({
   selector: 'app-by-region-page',
@@ -12,19 +28,26 @@ import { catchError, of } from 'rxjs';
   templateUrl: './by-region-page.component.html',
 })
 export class ByRegionPageComponent {
+  countryService = inject(CountryService);
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania', 'Antarctic'];
 
-  countryService = inject(CountryService);
+  /// router = inject(Router);
 
-  query = signal<Region | null>(null);
+  region = signal<Region | null>(null);
 
   countryResource = rxResource({
-    params: () => ({ query: this.query() }),
+    params: () => ({ region: this.region() }),
 
     stream: ({ params }) => {
-      if (!params.query) return of([]);
+      if (!params.region) return of([]);
 
-      return this.countryService.searchByRegion(params.query).pipe(
+      // this.router.navigate(['/country/by-region'], {
+      //   queryParams: {
+      //     region: params.region,
+      //   },
+      // });
+
+      return this.countryService.searchByRegion(params.region).pipe(
         catchError((err) => {
           console.log('Error Error', err);
 
--- src/app/country/pages/by-region-page/region-options/region-options.component.ts
+++ src/app/country/pages/by-region-page/region-options/region-options.component.ts
@@ -1,5 +1,30 @@
-import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  input,
+  linkedSignal,
+  output,
+  signal,
+} from '@angular/core';
 import { Region } from '../../../interfaces/country.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+
+function validateRegion(queryParam: string): Region {
+  queryParam = queryParam.toLowerCase();
+
+  const validRegion: Record<string, Region> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+
+  return validRegion[queryParam] ?? 'Americas';
+}
 
 @Component({
   selector: 'app-region-options',
@@ -8,14 +33,36 @@ import { Region } from '../../../interfaces/country.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegionOptionsComponent {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
   regionOptions = input<Region[]>([]);
 
-  selectedRegion = output<Region | null>();
+  selectedRegion = output<Region>();
 
-  currentActiveRegion = signal<Region | null>(null);
+  currentActiveRegion = linkedSignal<Region>(() => validateRegion(this.queryParam));
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.currentActiveRegion(); // this line trigger the effect since angular detects a signal inside its body
+
+    // const timeout = setTimeout(() => {
+    //   this.value.emit(value);
+    // }, 500);
+
+    // onCleanup(() => {
+    //   clearTimeout(timeout);
+    // });
+    console.log(value);
+    this.selectedRegion.emit(value);
+    this.router.navigate(['/country/by-region'], {
+      queryParams: {
+        region: value,
+      },
+    });
+  });
 
   onRegionSelected(region: Region) {
     this.currentActiveRegion.set(region);
-    this.selectedRegion.emit(region);
   }
 }
